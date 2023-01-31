1c126e3493f606dcde9b61668c7ea5f2ea3b4ddb
ABCganada <shinmg9712@gmail.com>
2023-01-31 23:22:06 +0900

    2-05 실습

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,10 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/Users/abcganada/projects/mysite/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi there!")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
