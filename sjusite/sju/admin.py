d516862ebab5fe0d976cdfe1f87819553fe041a3
ABCganada <shinmg9712@gmail.com>
2023-02-13 23:24:11 +0900

    admin page, createsuperuser

--- sjusite/sju/admin.py
+++ sjusite/sju/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Semester
 # Register your models here.
+
+class SemeseterAdmin(admin.ModelAdmin):
+    search_fields = ['table']
+
+admin.site.register(Semester, SemeseterAdmin)
--- sjusite/sju/models.py
+++ sjusite/sju/models.py
@@ -5,10 +5,11 @@ class Semester(models.Model):
     table = models.CharField(max_length=200)    # 시간표 이름
     credit = models.IntegerField()              # 이수 단위
     grade = models.FloatField()                 # 학점 평점
-    
+    create_at = models.DateTimeField()
     def __str__(self):
         return self.table
 
 class Subjects(models.Model):
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     content = models.TextField()
+    create_at = models.DateTimeField()
