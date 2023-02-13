b872411e543524e203a5e1db92c55e03130f13cb
ABCganada <shinmg9712@gmail.com>
2023-02-13 22:12:52 +0900

    sju project - model 생성

--- sjusite/config/settings.py
+++ sjusite/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sju.apps.SjuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- sjusite/sju/models.py
+++ sjusite/sju/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Semester(models.Model):
+    table = models.CharField(max_length=200)    # 시간표 이름
+    credit = models.IntegerField()              # 이수 단위
+    grade = models.FloatField()                 # 학점 평점
+    
+    def __str__(self):
+        return self.table
+
+class Subjects(models.Model):
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    content = models.TextField()
