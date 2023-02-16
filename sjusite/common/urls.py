8cf783353c025876bfd70f34c4cc8d0c566bb452
ABCganada <shinmg9712@gmail.com>
2023-02-16 17:20:16 +0900

    signup 기능 추가

--- sjusite/common/urls.py
+++ sjusite/common/urls.py
@@ -7,5 +7,5 @@ app_name = 'common'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup,name='sjginup'),
+    path('signup/', views.signup, name='signup'),
 ]
