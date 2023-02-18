a951fcd2aa4fb85602da2726b44a080dac69e253
ABCganada <shinmg9712@gmail.com>
2023-02-18 16:34:57 +0900

    password 입력창 수정

--- sjusite/common/forms.py
+++ sjusite/common/forms.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label='이메일')
+    name = forms.CharField(max_length=200, label='이름')
     
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", 'email')
+        fields = ("username", "name", "password1", "password2", 'email')
