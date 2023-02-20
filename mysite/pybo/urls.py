9bedf8bb2c3a339a3cf501b49ca0c740d954de5f
ABCganada <shinmg9712@gmail.com>
2023-02-20 17:35:58 +0900

    Question, Answer 추천 기능

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
         question_views.question_modify, name='question_modify'),
     path('question/delete/<int:question_id>/',
         question_views.question_delete, name="question_delete"),
+    path('question/vote/<int:question_id>/', 
+        question_views.question_vote, name='question_vote'),
     
     # answer_views.py
     path('answer/create/<int:question_id>/', 
@@ -25,4 +27,6 @@ urlpatterns = [
         answer_views.answer_modify, name="answer_modify"),
     path('answer/delete/<int:answer_id>/', 
         answer_views.answer_delete, name="answer_delete"),
+    path('answer/vote/<int:answer_id>/', 
+        answer_views.answer_vote, name='answer_vote'),
 ]
