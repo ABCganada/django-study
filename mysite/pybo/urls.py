5d8dd25964bc287882fd54140c4e248e3276d0e4
ABCganada <shinmg9712@gmail.com>
2023-02-20 16:02:21 +0900

    answer delete 기능

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
     path('answer/modify/<int:answer_id>/', views.answer_modify, name="answer_modify"),
+    path('answer/delete/<int:answer_id>/', views.answer_delete, name="answer_delete"),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -108,4 +108,14 @@ def answer_modify(request, answer_id):
     context = {'answer': answer, 'form': form}
     return render(request, 'pybo/answer_form.html', context)
 
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    
+    if request.user != answer.author:
+        messages.error(request, "삭제권한이 없습니다")
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
+
 
