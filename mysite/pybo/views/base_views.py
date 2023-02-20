01e4782a07cd1c41463360e2e24be1aa9eda05ad
ABCganada <shinmg9712@gmail.com>
2023-02-20 21:52:55 +0900

    검색창 기능

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,13 +1,25 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from ..models import Question
 
 def index(request):
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
+    
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(answer__content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+    
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
