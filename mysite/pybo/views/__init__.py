5c2de71f65363e48773253ef4a3e687b730c76b7
ABCganada <shinmg9712@gmail.com>
2023-02-20 16:50:56 +0900

    views 단순 파일 분리

---
+++ mysite/pybo/views/__init__.py
@@ -0,0 +1,3 @@
+from .base_views import *
+from .question_views import *
+from .answer_views import *
---
+++ mysite/pybo/views/base_views.py
@@ -0,0 +1,16 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from ..models import Question
+
+def index(request):
+    page = request.GET.get('page', '1')
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
