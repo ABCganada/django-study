3eae05cd711be244cb1de68a6a66e7cfa11e4a60
ABCganada <shinmg9712@gmail.com>
2023-03-02 16:41:36 +0900

    log test

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from ..models import Question
 
 def index(request):
+    3/0
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
